4b3932ac6cc9ef3c51f2e775c4fcaf1bbe8b6fbd
aashu <ashishbam07@gmail.com>
2023-04-13 06:07:28 +0530

    second file updated

---
+++ src/automated_simulator_system.java
@@ -0,0 +1,14 @@
+
+
+import java.io.IOException;
+
+public class automated_simulator_system extends OptionMenu{
+	
+	public static void main(String[] args) throws IOException
+	{
+		
+		OptionMenu optionMenu = new OptionMenu();
+		
+		optionMenu.getlogin();
+	}
+}
