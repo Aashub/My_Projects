52a49b3354e85bb287a2699157fd5bc0c026f750
aashu <ashishbam07@gmail.com>
2023-07-04 15:14:01 +0530

    remove file

--- band name generator.py
+++
@@ -1,8 +0,0 @@
-print("Welcome to the band name generator\n")
-
-city =input("what's the name of the city you grew up in?\n")
-pet = input("what's your pet's name?\n")
-
-band_name =print(city + pet+"\n")
-
-print("Your band name could be "+ band_name)
