4ab60c8aeea8bbc7730a3a2066a6a5c88507883f
ASHISH <86818694+Aashub@users.noreply.github.com>
2023-07-15 18:07:14 +0530

    Update main.py

--- 17.Turtle_Race/main.py
+++ 17.Turtle_Race/main.py
@@ -6,31 +6,6 @@ from turtle import Turtle, Screen
 turtles = Turtle()
 screen = Screen()
 
-# def move_forward():
-#     tim.forward(10)
-#
-# def move_backward():
-#     tim.backward(10)
-#
-# def move_anticlock():
-#     new_heading = tim.heading() + 10
-#     tim.setheading(new_heading)
-#
-# def move_clock():
-#     new_heading = tim.heading() - 10
-#     tim.setheading(new_heading)
-#
-# def clear_screen():
-#     screen.clearscreen()
-#
-#
-# screen.listen()
-#
-# screen.onkey(key = "w", fun = move_forward)
-# screen.onkey(key = "s", fun = move_backward)
-# screen.onkey(key = "a", fun = move_anticlock)
-# screen.onkey(key = "d", fun = move_clock)
-# screen.onkey(key = "c", fun = clear_screen)
 
 should_continue = False
 screen.setup(width= 500, height=400)
@@ -39,7 +14,6 @@ user_input = screen.textinput(title="make your bet", prompt= "decide the turtle 
 turtle_color = ["red", "blue", "green", "yellow", "violet"]
 y_axis = [-40,-10,20,50,80]
 turtle_list = []
-
 
 
 for index  in range(0,5):
