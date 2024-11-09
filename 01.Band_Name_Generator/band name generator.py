f9cd8c20c959c3c5667d0669ac43946c56be0054
ASHISH <86818694+Aashub@users.noreply.github.com>
2024-11-09 10:22:09 +0530

    Update band name generator.py

--- 01.Band_Name_Generator/band name generator.py
+++ 01.Band_Name_Generator/band name generator.py
@@ -1,8 +1,6 @@
-print("Welcome to the band name generator\n")
+print("Welcome to the Band Name Generator.")
+city_name = input("What's the name of the city you grow up in?\n")
 
-city =input("what's the name of the city you grew up in?\n")
-pet = input("what's your pet's name?\n")
+pet_name = input("What's your pet name?\n")
 
-band_name =print(city + pet+"\n")
-
-print("Your band name could be "+ band_name)
+print("Your brand name could be " +city_name+ " " + pet_name)
