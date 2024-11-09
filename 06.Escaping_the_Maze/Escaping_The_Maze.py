4bb7170ac8fac99910334c7f7d0a0532740a166f
Anony <ashishbam07@gmail.com>
2024-11-09 12:49:01 +0530

    fist commit

---
+++ 06.Escaping_the_Maze/Escaping_The_Maze.py
@@ -0,0 +1,20 @@
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json
+
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+#
+#
+# def robot():
+#     if right_is_clear():
+#         turn_right()
+#         move()
+#     elif wall_in_front():
+#         turn_left()
+#     elif wall_on_right():
+#         move()
+#
+#
+# while not at_goal():
+#     robot()
